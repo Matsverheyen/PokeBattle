79130fd046df5bcb7a958a0471df3e8378b8f607
Mats <99052518@mydavinci.nl>
2020-02-17 18:51:08 +0100

    attacks

--- Pokemon.php
+++ Pokemon.php
@@ -16,9 +16,36 @@ class Pokemon {
     $this->resistance = $resistance;
   }
 
+  public function getWeakness() {
+    return $this->weakness;
+  }
+
+  public function getResistance() {
+    return $this->resistance;
+  }
 
   public function getDetails() {
     return json_encode($this);
+  }
+
+  public function attack($move, $target) {
+    echo $this->name . " Hp: " . $this->hitpoints . '<br>';
+    echo $target->name . " Hp: " . $target->hitpoints . '<br><br>';
+    $index = array_search($move, array_column($this->attacks, 'move'));
+    $damage = $this->attacks[$index]->damage;
+    $weakness = $target->getWeakness();
+    $resistance = $target->getResistance();
+    if ($this->energyType === $weakness->type) {
+      $damage = $damage*$weakness->mulitplier;
+      $target->hitpoints = $target->hitpoints - $damage;
+    } elseif ($this->energyType === $resistance->type) {
+      $damage = $damage/$resistance->mulitplier;
+      $target->hitpoints = $target->hitpoints - $damage;
+    } else {
+      $target->hitpoints = $target->hitpoints - $damage;
+    }
+    echo "<strong>" . $this->name . "</strong> Attacked <strong>" . $target->name . "</strong> with: <strong>" . $move . "</strong> and did <strong>" . $damage . "</strong> Damage!<br>";
+    echo $target->name . ' New hp: ' . $target->hitpoints . "<br><br>";
   }
 
 }
--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 include 'Pokemon.php';
 
-$pikachu = new Pokemon('Pikachu', 'Lightning', 60, [array('move' => 'Electric Ring','damage' => 50), array('move' => 'Pika Punch','damage' => 20) ], array('type' => 'Fire', 'mulitplier' => 1.5), array('type' => 'Fighting', 'mulitplier' => 20));
-$charmeleon = new Pokemon('Charmeleon', 'Fire', 60, [array('move' => 'Head Butt','damage' => 10), array('move' => 'Flare','damage' => 30) ], array('type' => 'Water', 'mulitplier' => 2), array('type' => 'Lightning', 'mulitplier' => 10));
+$pikachu = new Pokemon('Pikachu', 'Lightning', 60, [(object)array('move' => 'Electric Ring','damage' => 50), (object)array('move' => 'Pika Punch','damage' => 20) ], (object)array('type' => 'Fire', 'mulitplier' => 1.5), (object)array('type' => 'Fighting', 'mulitplier' => 20));
+$charmeleon = new Pokemon('Charmeleon', 'Fire', 60, [(object)array('move' => 'Head Butt','damage' => 10), (object)array('move' => 'Flare','damage' => 30) ], (object)array('type' => 'Water', 'mulitplier' => 2), (object)array('type' => 'Lightning', 'mulitplier' => 10));
 echo $pikachu->getDetails();
 echo '<br><br>';
 echo $charmeleon->getDetails();
+echo '<br><br>';
+$pikachu->attack('Pika Punch', $charmeleon);
+$charmeleon->attack('Flare', $pikachu);
